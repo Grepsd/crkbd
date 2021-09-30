a5a7a4dd840ddf4aa47136962331852cc8dfe2a0
Grepsd <grepsd@gmail.com>
2021-09-30 16:08:23 +0200

    updating layout and disabling rgb

--- keymap.c
+++ keymap.c
@@ -32,10 +32,10 @@ enum custom_keycodes {
   RAISE,
   ADJUST,
   RGBRST,
-  KC_RACL // right alt / colon
+  KC_LACL // right alt / colon
 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RAPC, LGUI_T(KC_ESC), RAISE, KC_SPC, KC_ENT, LOWER, LALT(KC_COLN)),
+	[0] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RAPC, LGUI_T(KC_ESC), RAISE, KC_SPC, KC_ENT, LOWER, KC_LACL),
 	[1] = LAYOUT_split_3x6_3(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL, KC_LCTL, KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_LSFT, KC_NO, KC_BTN2, KC_WH_D, KC_WH_U, KC_BTN1, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, KC_RALT, KC_LGUI, RAISE, KC_SPC, KC_ENT, LOWER, KC_RALT),
 	[2] = LAYOUT_split_3x6_3(KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, KC_LCTL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_MINS, KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV, KC_LSFT, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD, KC_LGUI, LOWER, KC_SPC, KC_ENT, RAISE, KC_RALT),
 	[3] = LAYOUT_split_3x6_3(RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC__MUTE, KC__VOLDOWN, KC__VOLUP, KC_NO, KC_NO, RGB_TOG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PAUS, KC_MPLY, KC_MPRV, KC_MNXT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_SLCK, KC_NO, KC_MRWD, KC_MFFD, KC_NO, KC_NO, KC_LGUI, LOWER, KC_SPC, KC_ENT, RAISE, KC_RALT)
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------| */
 /*     KC_LSPO,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                   KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_RSPC, */
 /*   //|------+------+------+------+------+------+------|  |------+------+------+-------+------+-------+--------| */
-/*                                KC_LGESC,LOWER, KC_SPC,   RCTL_T(KC_ENT), RAISE, KC_RACL */
+/*                                KC_LGESC,LOWER, KC_SPC,   RCTL_T(KC_ENT), RAISE, KC_LACL */
 /*                               //`--------------------'  `--------------------' */
 /*   ), */
 /*  */
@@ -358,7 +358,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           layer_off(_ADJUST);
         }
         return false;
-    case KC_RACL:
+    case KC_LACL:
         if (record->event.pressed) {
           my_colon_timer = timer_read();
           register_code(KC_RALT);
